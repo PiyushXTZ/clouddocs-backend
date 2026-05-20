173ba244e12be33d48039fd041432942bece1033
PiyushXTZ <piyushnishadraj777@gmail.com>
2026-05-20 23:12:13 +0530

    updated code as per aws

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -1,15 +1,26 @@
-const path = require('path');
+const { PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+const s3 = require('../config/s3');
 const { createFile, getFilesByProject, getFileById, deleteFile } = require('../models/fileModel');
 const logActivity = require('../utils/activityLogger');
 
 const upload = async (req, res) => {
   try {
-    const { originalname, filename, size, mimetype } = req.file;
-    const fileUrl = `/uploads/${filename}`;
+    const { originalname, buffer, mimetype, size } = req.file;
+    const filename = `${Date.now()}-${originalname}`;
+
+    await s3.send(new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: filename,
+      Body: buffer,
+      ContentType: mimetype,
+    }));
+
+    const fileUrl = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${filename}`;
     const fileId = await createFile(filename, originalname, fileUrl, size, mimetype, req.params.id, req.user.id);
     await logActivity(req.user.id, req.params.id, `uploaded ${originalname}`);
     res.status(201).json({ id: fileId, fileUrl, originalname });
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: 'Server error' });
   }
 };
@@ -27,6 +38,12 @@ const remove = async (req, res) => {
   try {
     const file = await getFileById(req.params.fileId);
     if (!file) return res.status(404).json({ message: 'File not found' });
+
+    await s3.send(new DeleteObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: file.filename,
+    }));
+
     await deleteFile(req.params.fileId);
     await logActivity(req.user.id, file.project_id, `deleted ${file.original_name}`);
     res.json({ message: 'File deleted' });
